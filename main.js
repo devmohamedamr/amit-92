5b47b9911d7b4ca5243cf7c067e538643b99622c
mohamed amr <dev.mohamedamr@gmail.com>
2023-03-05 19:45:06 +0200

    js

---
+++ main.js
@@ -0,0 +1,44 @@
+
+// document.getElementById("click").onclick = function(){
+//        var pas =  document.getElementById("password")
+
+//        if(pas.type == "text"){
+//             document.getElementById("click").innerText = "show"
+//             pas.type = "password"
+//        }else{
+//         document.getElementById("click").innerText = "hide"
+
+//             pas.type = "text"
+//        }
+// }
+
+
+
+
+
+// var username = "ahmed"
+// console.log(username[1])
+
+
+
+// document.getElementById("text").onkeyup = function(){
+//     var len = document.getElementById("text").value.length
+//     if(len>=20){
+//         document.getElementById("text").classList.add("textchange")
+//     }else{
+//         document.getElementById("text").classList.remove("textchange")
+ 
+//     }
+//     document.getElementById("res").innerText = "count is : "+len
+// }
+
+
+
+
+
+
+
+function chnageColor (color)
+{
+    document.body.style.background = color
+}
